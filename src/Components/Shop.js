b116ad079f44c6ca1d712504a2124ace34f4da34
Samay Joshi <63327946+samayjoshi23@users.noreply.github.com>
2022-02-20 15:11:01 +0530

    first commit

---
+++ src/Components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Shop() {
+  return (
+    <div className='container my-5'>
+        <h2>Adidas Shoes : 50Rs</h2>
+      <button className="btn btn-primary mx-2">-</button>
+        Add to Cart
+      <button className="btn btn-primary mx-2">+</button>
+    </div>
+  )
+}
+
+export default Shop
