68b6a0394e2b0981a0accd64764b365f90def834
Samay Joshi <63327946+samayjoshi23@users.noreply.github.com>
2022-02-21 20:15:51 +0530

    completed redux application

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from '../State/index';
 
 function Shop() {
+
+  const dispatch = useDispatch();
+  const {depositeMoney, withdrawMoney} = bindActionCreators(actionCreators, dispatch);
+
   return (
     <div className='container my-5'>
-        <h2>Adidas Shoes : 50Rs</h2>
-      <button className="btn btn-primary mx-2">-</button>
-        Add to Cart
-      <button className="btn btn-primary mx-2">+</button>
+        <h2>Deposit / Withdraw money</h2>
+        <button className="btn btn-primary mx-2" onClick={()=>{withdrawMoney(200)}}>-</button>
+            Update Balance
+        <button className="btn btn-primary mx-2 my-5" onClick={()=>{depositeMoney(500)}}>+</button>
     </div>
   )
 }
